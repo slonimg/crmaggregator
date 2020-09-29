1b1a46d11c90fd54f66865734f56d59cfd90fd40
Gilad Slonim <gslonim@grubhub.com>
2020-09-29 23:19:50 +0300

    basic web service

---
+++ src/main/java/com/example/demo/AggregatorApiImpl.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class AggregatorApiImpl implements AggregatorApi {
+
+    @Override
+    public List<SupportCase> get(String product) {
+        System.out.println(product);
+
+        SupportCase supportCase = new SupportCase(1, "BLUE", 123, "456", 789, "Open",
+                Date.from(Instant.now().minusSeconds(2592000)), Date.from(Instant.now()));
+        List<SupportCase> result = new ArrayList<>();
+        result.add(supportCase);
+        return result;
+    }
+}
